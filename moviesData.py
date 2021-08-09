3d491134ee92e794d6d58ecec5ae0044f27ccd76
MMaduranga <m.maduranga433@gmail.com>
2021-08-09 16:00:05 +0530

    read the data sheet and show the dataframe,shape of data frame,columns of dataframe in streamlit webapp

---
+++ moviesData.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+
+def read_data():
+    dataframe = pd.read_csv("C:\\Users\\MasterPC\\Downloads\\imdb_movies_0.csv")
+    pd.set_option('max_columns', 11)
+    return dataframe
+
+
+def webapp_view(*data):
+    for i in data:
+        st.write(i)
+
+
+def sidebar():
+    st.sidebar.header('SideBar')
+    if st.sidebar.checkbox('Show Source Data & Details'):
+        dataframe = read_data()
+        webapp_view('\n\nMovies Table', dataframe, "Shape of Table", dataframe.shape, 'Columns of the table',
+                    dataframe.columns)
+
+
+st.header("Movies")
+sidebar()
+
+
+
+
